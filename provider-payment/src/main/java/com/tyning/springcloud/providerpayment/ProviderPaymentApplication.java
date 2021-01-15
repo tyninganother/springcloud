8579aa53c05811fafe450080bc89d76582f2927a
tyninganother <tyning.another@gmail.com>
2021-01-15 11:17:29 +0800

    Initial commit

---
+++ provider-payment/src/main/java/com/tyning/springcloud/providerpayment/ProviderPaymentApplication.java
@@ -0,0 +1,11 @@
+package com.tyning.springcloud.providerpayment;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderPaymentApplication {
+    public static void main(String[] args) {
+
+    }
+}
